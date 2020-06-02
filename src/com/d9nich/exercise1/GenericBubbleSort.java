81c7c3e15a05198b593a3c716dd5aba1879d76cf
Danylo Halaiko <dan.galaiko@gmail.com>
2020-06-02 13:39:06 +0300

    Exercise1 has been done. The first method sorts the elements using the Comparable interface, and the second uses the Comparator interface.

---
+++ src/com/d9nich/exercise1/GenericBubbleSort.java
@@ -0,0 +1,45 @@
+package com.d9nich.exercise1;
+
+import java.util.Comparator;
+
+public class GenericBubbleSort {
+    public static <E extends Comparable<E>>
+    void bubbleSort(E[] list) {
+        boolean needNextPass = true;
+
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            // Array may be sorted and next pass not needed
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i].compareTo(list[i + 1]) > 0) {
+                    // Swap list[i] with list[i + 1]
+                    E temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true; // Next pass still needed
+                }
+            }
+        }
+    }
+
+    public static <E> void bubbleSort(E[] list,
+                                      Comparator<? super E> comparator) {
+        boolean needNextPass = true;
+
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            // Array may be sorted and next pass not needed
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (comparator.compare(list[i], list[i + 1]) > 0) {
+                    // Swap list[i] with list[i + 1]
+                    E temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true; // Next pass still needed
+                }
+            }
+        }
+    }
+}
